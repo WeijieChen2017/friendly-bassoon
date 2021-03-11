5596dcfddba0432ad2c5ea316c2a8889b327af96
WeijieChen2017 <weijiechen1994@gmail.com>
2021-03-10 22:03:27 -0600

    hybrid

--- z1_generate_dataset.py
+++ z1_generate_dataset.py
@@ -42,7 +42,7 @@ def main():
         print("@"*60)
         print(nii_path)
         nii_file = nib.load(nii_path)
-        nii_name = os.path.basename(nii_file)
+        nii_name = os.path.basename(nii_path)
         nii_name = os.path.splittext(nii_name)[0]
         nii_header = nii_file.header
         nii_affine = nii_file.affine
