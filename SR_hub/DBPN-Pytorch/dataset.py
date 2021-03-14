428e9009a127c3904128e423799ee82c32655d54
WeijieChen2017 <weijiechen1994@gmail.com>
2021-03-13 18:44:03 -0600

    hybrid

--- SR_hub/DBPN-Pytorch/dataset.py
+++ SR_hub/DBPN-Pytorch/dataset.py
@@ -25,8 +25,8 @@ def rescale_img(img_in, scale):
     return zoom(img_in, zoom=(scale, scale, 1))
 
 def get_patch(img_in, img_tar, img_bic, patch_size, scale, ix=-1, iy=-1):
-    (ih, iw) = img_in.size
-    (th, tw) = (scale * ih, scale * iw)
+    [ih, iw, _] = img_in.shape
+    [th, tw] = [scale * ih, scale * iw]
 
     patch_mult = scale #if len(scale) > 1 else 1
     tp = patch_mult * patch_size
@@ -37,37 +37,38 @@ def get_patch(img_in, img_tar, img_bic, patch_size, scale, ix=-1, iy=-1):
     if iy == -1:
         iy = random.randrange(0, ih - ip + 1)
 
-    (tx, ty) = (scale * ix, scale * iy)
+    [tx, ty] = [scale * ix, scale * iy]
 
-    img_in = img_in.crop((iy,ix,iy + ip, ix + ip))
-    img_tar = img_tar.crop((ty,tx,ty + tp, tx + tp))
-    img_bic = img_bic.crop((ty,tx,ty + tp, tx + tp))
+    img_in = img_in[iy,ix,iy + ip, ix + ip]
+    img_tar = img_tar[ty,tx,ty + tp, tx + tp]
+    img_bic = img_bic[ty,tx,ty + tp, tx + tp]
                 
     info_patch = {
         'ix': ix, 'iy': iy, 'ip': ip, 'tx': tx, 'ty': ty, 'tp': tp}
 
     return img_in, img_tar, img_bic, info_patch
 
-def augment(img_in, img_tar, img_bic, flip_h=True, rot=True):
+def augment(img_in, img_tar, img_bic, flip_h=True, flip_v=True, rot=True):
     info_aug = {'flip_h': False, 'flip_v': False, 'trans': False}
     
     if random.random() < 0.5 and flip_h:
-        img_in = ImageOps.flip(img_in)
-        img_tar = ImageOps.flip(img_tar)
-        img_bic = ImageOps.flip(img_bic)
+        for img in [img_in, img_tar, img_bic]:
+            for idx_c in range(0):
+                img[:, :, idx_c] = np.fliplr(img)
         info_aug['flip_h'] = True
 
+    if random.random() < 0.5 and flip_v:
+        for img in [img_in, img_tar, img_bic]:
+            for idx_c in range(0):
+                img[:, :, idx_c] = np.flipup(img)
+        info_aug['flip_v'] = True
+
     if rot:
-        if random.random() < 0.5:
-            img_in = ImageOps.mirror(img_in)
-            img_tar = ImageOps.mirror(img_tar)
-            img_bic = ImageOps.mirror(img_bic)
-            info_aug['flip_v'] = True
-        if random.random() < 0.5:
-            img_in = img_in.rotate(180)
-            img_tar = img_tar.rotate(180)
-            img_bic = img_bic.rotate(180)
-            info_aug['trans'] = True
+        cnt_rot = int(random.random()//0.25):
+        for img in [img_in, img_tar, img_bic]:
+            for idx_c in range(0):
+                img[:, :, idx_c] = np.rot90(img, cnt_rot)
+        info_aug['trans'] = True
             
     return img_in, img_tar, img_bic, info_aug
     
@@ -87,7 +88,6 @@ class DatasetFromFolder(data.Dataset):
         # print(target.shape, self.image_filenames[index][:-6]+"_Y.npy")
         # input = target.resize((int(target.size[0]/self.upscale_factor),int(target.size[1]/self.upscale_factor)), Image.BICUBIC)       
         bicubic = rescale_img(origin, self.upscale_factor)
-        print(bicubic.shape)
         
         origin, target, bicubic, _ = get_patch(origin,target,bicubic,self.patch_size, self.upscale_factor)
         
