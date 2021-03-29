d5625d56a56c138106db4f24d77fb2d79d9582b2
WeijieChen2017 <weijiechen1994@gmail.com>
2021-03-29 03:14:18 -0500

    hybrid

--- z5_pixel_alignment.py
+++ z5_pixel_alignment.py
@@ -1,10 +1,17 @@
 import os
 import glob
 import nibabel as nib
+import numpy as np
 import nibabel.processing
 
 mri_list = glob.glob("./data/mri_4688/*.nii.gz")
 mri_list.sort()
+
+def maxmin_norm(data):
+    MAX = np.amax(data)
+    MIN = np.amin(data)
+    data = (data - MIN)/(MAX-MIN)
+    return data
 
 for mri_path in mri_list:
     print("&"*60)
@@ -16,15 +23,18 @@ for mri_path in mri_list:
     mri_file = nib.load(mri_path)
     mri_file_4x = nib.processing.conform(mri_file, out_shape=(960, 960, 284), voxel_size=(0.25, 0.25, 0.6))
     save_name = mri_dir + mri_name + "_250.nii.gz"
-    nib.save(mri_file_4x, save_name)
-    print(save_name)
     
     mri_smooth = nib.processing.smooth_image(mri_file_4x, fwhm=3)
     save_name = mri_dir + mri_name + "_25f.nii.gz"
-    nib.save(mri_smooth, save_name)
-    print(save_name)
     
     mri_smooth_1x = nib.processing.conform(mri_smooth, out_shape=(240, 240, 71), voxel_size=(1, 1, 2.4))
     save_name = mri_dir + mri_name + "_1f3.nii.gz"
-    nib.save(mri_smooth_1x, save_name)
-    print(save_name)
+
+    for package in [[mri_file_4x, "250"], [mri_smooth, "25f"], [mri_smooth_1x, "1f3"]]
+        nii_file = package[0]
+        tag = package[1]
+
+        nii_new_file = nib.Nifti1Image(maxmin_norm(nii_file.get_fdata()), nii_file.affine, nii_file.header)
+        save_name = mri_dir + mri_name + "_" + tag + ".nii.gz"
+        nib.save(nii_new_file, save_name)
+        print(save_name)
