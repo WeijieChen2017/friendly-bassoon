97440f64e48b242b25ad941053eefa406c069de9
WeijieChen2017 <weijiechen1994@gmail.com>
2021-04-06 14:51:39 -0500

    hybrid

--- SR_hub/DBPN-Pytorch/main.py
+++ SR_hub/DBPN-Pytorch/main.py
@@ -247,7 +247,7 @@ else:
     model = DBPN(num_channels=3, base_filter=64,  feat = 256, num_stages=7, scale_factor=opt.upscale_factor) 
     
 model = torch.nn.DataParallel(model, device_ids=gpus_list)
-criterion = nn.L2Loss()
+criterion = nn.MSELoss()
 
 print('---------- Networks architecture -------------')
 print_network(model)
