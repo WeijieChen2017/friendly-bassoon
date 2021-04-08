b6d5a81f290dd7cb12db8e5785cf5aeaf1167169
WeijieChen2017 <weijiechen1994@gmail.com>
2021-04-08 10:11:21 -0500

    hybrid

--- z5_pixel_alignment_mri.py
+++ z5_pixel_alignment_mri.py
@@ -4,7 +4,7 @@ import nibabel as nib
 import numpy as np
 import nibabel.processing
 
-mri_list = glob.glob("./data/mri_4688/*.nii.gz")
+mri_list = glob.glob("./data/nick_MRI/*.nii.gz")
 mri_list.sort()
 
 def maxmin_norm(data):
@@ -22,14 +22,14 @@ for mri_path in mri_list:
     mri_file = nib.load(mri_path)
     scale_factor = np.amax(mri_file.get_fdata())
     
-    file_1 = nib.processing.conform(mri_file, out_shape=(960, 960, 71), voxel_size=(0.25, 0.25, 2.4))
-    name_1 = "x960y960z71"
+    file_1 = nib.processing.conform(mri_file, out_shape=(960, 960, 68), voxel_size=(0.25, 0.25, 2.4))
+    name_1 = "x250y250z2400"
     
     file_2 = nib.processing.smooth_image(file_1, fwhm=3)
-    name_2 = "x960y960z71f3"
+    name_2 = "x250y250z2400f3"
     
-    file_3 = nib.processing.conform(file_2, out_shape=(240, 240, 71), voxel_size=(1, 1, 2.4))
-    name_3 = "x240y240z71f3"
+    file_3 = nib.processing.conform(file_2, out_shape=(240, 240, 68), voxel_size=(1, 1, 2.4))
+    name_3 = "x1000y1000z2400f3"
 
     for package in [[file_1, name_1], [file_2, name_2], [file_3, name_3]]:
         nii_file = package[0]
