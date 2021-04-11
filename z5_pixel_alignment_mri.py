b3950d5444865426e080ae4415ec5731e8767221
WeijieChen2017 <weijiechen1994@gmail.com>
2021-04-10 23:02:11 -0500

    hybrid

--- z5_pixel_alignment_mri.py
+++ z5_pixel_alignment_mri.py
@@ -4,7 +4,7 @@ import nibabel as nib
 import numpy as np
 import nibabel.processing
 
-mri_list = glob.glob("./data/eval_MRI/*_INV.nii.gz")
+mri_list = glob.glob("./data/mri_4688/*.nii.gz")
 mri_list.sort()
 
 def maxmin_norm(data):
