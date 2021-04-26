2c3a04942068733691d68d78f472fc89954ade41
WeijieChen2017 <weijiechen1994@gmail.com>
2021-04-26 00:03:30 -0500

    hybrid

---
+++ y2_pve2mat.py
@@ -0,0 +1,25 @@
+from scipy.ndimage import binary_erosion, binary_dilation
+from scipy.io import savemat
+from nibabel import processing
+import nibabel as nib
+import numpy as np
+import glob
+import os
+
+file_list = glob.glob("pve*.nii.gz")
+file_list.sort()
+for file_path in file_list:
+    print("*"*50)
+    print(file_path)
+    file_name = os.path.basename(file_path)[:-7]
+    print(file_name)
+    mri_file = nib.load(file_path)
+    mri_data = mri_file.get_fdata()
+    print(mri_data.shape)
+
+    mdic = {"data": mri_data}
+
+    save_name += nii_name[:-7]+".mat"
+    savemat(save_name, mdic)
+    print("Mat:", save_name)
+    print("--------------------------------------")
