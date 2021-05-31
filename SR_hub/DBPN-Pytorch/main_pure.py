7bc4166a052273fe93792d5c5ea42cd4b13eebc4
WeijieChen2017 <weijiechen1994@gmail.com>
2021-05-31 09:13:42 -0500

    hybrid

--- SR_hub/DBPN-Pytorch/main_pure.py
+++ SR_hub/DBPN-Pytorch/main_pure.py
@@ -174,13 +174,13 @@ def train(epoch):
 
             loss = criterion(prediction, target)
 
-            # l1_lambda = 1e-7
-            l2_lambda = 1e-6
-            # l1_penalty = sum(p.abs().sum() for p in model.parameters())
-            l2_penalty = sum((p**2).sum() for p in model.parameters())
+            l1_lambda = 1e-6
+            # l2_lambda = 1e-6
+            l1_penalty = sum(p.abs().sum() for p in model.parameters())
+            # l2_penalty = sum((p**2).sum() for p in model.parameters())
 
-            # loss = loss + l1_lambda*l1_penalty
-            loss = loss + l2_lambda*l2_penalty
+            loss = loss + l1_lambda*l1_penalty
+            # loss = loss + l2_lambda*l2_penalty
 
             t1 = time.time()
             epoch_loss += loss.data
