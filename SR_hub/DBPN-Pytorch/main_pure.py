19b67ed10440f25bfe6d968c683a74fa599cb19d
WeijieChen2017 <weijiechen1994@gmail.com>
2021-05-31 23:40:05 -0500

    hybrid

--- SR_hub/DBPN-Pytorch/main_pure.py
+++ SR_hub/DBPN-Pytorch/main_pure.py
@@ -45,7 +45,7 @@ parser.add_argument('--pretrained_sr', default='DBPN-RES-MR64-3_4x.pth', help='s
 # parser.add_argument('--pretrained_sr', default='DBPN-RES-MR64-3_4x.pth', help='sr pretrained base model')
 parser.add_argument('--pretrained', type=bool, default=True)
 parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
-parser.add_argument('--prefix', default='SEE5_L11e6_', help='Location to save checkpoint models')
+parser.add_argument('--prefix', default='SEE5_', help='Location to save checkpoint models')
 
 opt = parser.parse_args()
 gpus_list = range(opt.gpus)
@@ -174,12 +174,12 @@ def train(epoch):
 
             loss = criterion(prediction, target)
 
-            l1_lambda = 1e-6
+            # l1_lambda = 1e-6
             # l2_lambda = 1e-6
-            l1_penalty = sum(p.abs().sum() for p in model.parameters())
+            # l1_penalty = sum(p.abs().sum() for p in model.parameters())
             # l2_penalty = sum((p**2).sum() for p in model.parameters())
 
-            loss = loss + l1_lambda*l1_penalty
+            # loss = loss + l1_lambda*l1_penalty
             # loss = loss + l2_lambda*l2_penalty
 
             t1 = time.time()
