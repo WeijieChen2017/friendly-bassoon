0524e078584df570d27ddfa64495924564302f80
WeijieChen2017 <weijiechen1994@gmail.com>
2021-08-09 01:23:57 -0500

    hybrid

--- x1_pet2ct.py
+++ x1_pet2ct.py
@@ -24,6 +24,8 @@ for pet_path in pet_list:
     print(pet_file.shape, np.amax(pet_file), np.amin(pet_file))
     print(ct_file.shape, np.amax(ct_file), np.amin(ct_file))
     
+    pet_big = nib.processing.conform(pet_blur, out_shape=(512, 512, 64), voxel_size=(1.367, 1.367, 3.27))
+    name_big = "NACB"
     # name_0 = "PF3_ORI"
 
     # pet_1x = nib.processing.conform(pet_blur, out_shape=(300, 300, 103), voxel_size=(1, 1, 2.4))
@@ -32,11 +34,11 @@ for pet_path in pet_list:
     # pet_4x = nib.processing.conform(pet_blur, out_shape=(1200, 1200, 103), voxel_size=(0.25, 0.25, 2.4))
     # name_2 = "PF3_x250y250z2400"
 
-    # for package in [[pet_file, name_0], [pet_1x, name_1], [pet_4x, name_2]]:
-    #     nii_file = package[0]
-    #     tag = package[1]
+    for package in [[pet_big, name_big]]:
+        nii_file = package[0]
+        tag = package[1]
 
-    #     nii_new_file = nib.Nifti1Image(maxmin_norm(nii_file.get_fdata()), nii_file.affine, nii_file.header)
-    #     save_name = pet_dir + pet_name + "_" + tag + ".nii.gz"
-    #     nib.save(nii_new_file, save_name)
-    #     print(save_name)
+        nii_new_file = nib.Nifti1Image(maxmin_norm(nii_file.get_fdata()), nii_file.affine, nii_file.header)
+        save_name = pet_dir + pet_name + "_" + tag + ".nii.gz"
+        nib.save(nii_new_file, save_name)
+        print(save_name)
